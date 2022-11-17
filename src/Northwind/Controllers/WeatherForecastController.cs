5b03f135c2d63505c4a84f4d9f3fd0720a059d90
Edgar Flores <efo.ms@live.com.mx>
2022-11-16 19:49:32 -0600

    Testing pipeline by adding controller endpoint

--- src/Northwind/Controllers/WeatherForecastController.cs
+++ src/Northwind/Controllers/WeatherForecastController.cs
@@ -29,4 +29,10 @@ public class WeatherForecastController : ControllerBase
         })
         .ToArray();
     }
+
+    [HttpGet(Name = "GetFreezingWeatherForecast")]
+    public WeatherForecast GetFirstWeatherForecast()
+    {
+        return Summaries[0];
+    }
 }
